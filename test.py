8abadb20de3bd3abe09587cacdb62d3b3f26017d
xabertum <mohatar@gmail.com>
2016-02-13 13:56:38 +0100

    Added Custom Colors

--- test.py
+++ test.py
@@ -1,7 +1,9 @@
+from UHScroll_custom_colors import *
 from UHScroll import *
 import time
 
-unicorn_scroll_lowercase('     hola bro!','red',200,0.2)
+unicorn_scroll_lowercase_rgb('hola bro!',55,55,55,0.2)
 time.sleep(1)
-unicorn_scroll_uppercase('Raspberry Pi','red',200,0.1)
+
+unicorn_scroll_uppercase('hola bro!','red',50,0.05)
 time.sleep(1)

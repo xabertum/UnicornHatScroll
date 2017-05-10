1c6e905412faf2738610ebdc3ade8f71b429bbbc
xabertum <mohatar@yahoo.com>
2017-05-10 21:06:48 +0200

    minor cahgnes

--- test.py
+++ test.py
@@ -2,8 +2,8 @@ from UHScroll_custom_colors import *
 from UHScroll import *
 import time
 
-unicorn_scroll_lowercase_rgb('    hola bro! 123',255,255,255,0.2)
+unicorn_scroll_lowercase_rgb(' hola bro! 123',255,255,255,0.9)
 time.sleep(1)
 
-unicorn_scroll_uppercase('hola bro!','red',250,0.05)
+unicorn_scroll_uppercase(' hola bro!','red',250,0.05)
 time.sleep(1)
